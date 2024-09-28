a359a746ccf8bb2e93b0ceadf3efc8d7a3c3714e
kevin <kevin.jessid@gmail.com>
2024-09-28 16:31:09 -0500

    fixing hostname part x1

--- script.sh
+++ script.sh
@@ -70,15 +70,12 @@ box-shadow: 0 0 5px #0aafe6, 0 0 5px #ffffff;
 EOF
 # hostname info
 while read line; do
-    if [[ $line != "<td>HOSTNAMESTRING</td>" ]]
-        then
-            echo $line >> /var/www/html/index.html
-        else
-            echo "<td>$(hostname -i)</td>" >> /var/www/html/index.html
-            elif [[ $line != "<td>IDSTRING</td>" ]]
-                then
-                    echo "<td>$(cat /dev/urandom |tr -dc 'a-zA-Z' |head -c 3; cat /dev/urandom |tr -dc '0-9' |head -c 3)</td>" >> /var/www/html/index.html
-            fi
-        fi
+    if [[ $line == "<td>HOSTNAMESTRING</td>" ]]; then
+        echo "<td>$(hostname -i)</td>" >> /var/www/html/index.html
+        elif [[ $line != "<td>IDSTRING</td>" ]]; then
+            echo "<td>$(cat /dev/urandom |tr -dc 'a-zA-Z' |head -c 3; cat /dev/urandom |tr -dc '0-9' |head -c 3)</td>" >> /var/www/html/index.html
+            else
+                echo $line >> /var/www/html/index.html
+    fi
 done < $HOME/template.txt
 rm -f $HOME/template.txt               
