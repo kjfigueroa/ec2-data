5a34d190bdb35aa5f127cd33f6e98098c6b4b3a5
kevin <kevin.jessid@gmail.com>
2024-09-28 16:39:51 -0500

    fixing hostname part x3

--- script.sh
+++ script.sh
@@ -70,13 +70,24 @@ box-shadow: 0 0 5px #0aafe6, 0 0 5px #ffffff;
 EOF
 # hostname info
 while read line; do
-    if [[ $line == "<td>HOSTNAMESTRING</td>" ]]; then
-        echo "<td>$(hostname -i)</td>" >> /var/www/html/index.html
-        elif [[ $line != "<td>IDSTRING</td>" ]]; then
-            echo "<td>$(cat /dev/urandom |tr -dc 'a-zA-Z' |head -c 3; cat /dev/urandom |tr -dc '0-9' |head -c 3)</td>" >> /var/www/html/index.html
-            else
-                echo $line >> /var/www/html/index.html
+    if [[ $line != "<td>HOSTNAMESTRING</td>" ]] || [[ $line != "<td>IDSTRING</td>" ]]; then
+        echo $line >> /var/www/html/index.html
+        elif [[ $line == "<td>HOSTNAMESTRING</td>" ]]; then
+            echo "<td>$(hostname -i)</td>" >> /var/www/html/index.html
+            elif [[ $line != "<td>IDSTRING</td>" ]]; then
+                echo "<td>$(cat /dev/urandom |tr -dc 'a-zA-Z' |head -c 3; cat /dev/urandom |tr -dc '0-9' |head -c 3)</td>" >> /var/www/html/index.html
+            fi
         fi
     fi
+
+# while read line; do
+#     if [[ $line == "<td>HOSTNAMESTRING</td>" ]]; then
+#         echo "<td>$(hostname -i)</td>" >> /var/www/html/index.html
+#         elif [[ $line != "<td>IDSTRING</td>" ]]; then
+#             echo "<td>$(cat /dev/urandom |tr -dc 'a-zA-Z' |head -c 3; cat /dev/urandom |tr -dc '0-9' |head -c 3)</td>" >> /var/www/html/index.html
+#             else
+#                 echo $line >> /var/www/html/index.html
+#         fi
+#     fi
 done < $HOME/template.txt
 rm -f $HOME/template.txt               
